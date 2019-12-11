c696f8b604feaf9f41d499f8b729b37546b9d0ac
Pearl Molefe <peamo@hotmail.com>
2019-12-11 11:42:59 +0200

    animate counter

--- Classifieds/wwwroot/js/site.js
+++ Classifieds/wwwroot/js/site.js
@@ -465,5 +465,18 @@ $(document).ready(function () {
         }
         
     });
+   //=============================================================
    
+    $('.counter-count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+                duration: 5000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+    });
+
 });
